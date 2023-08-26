7900844bd0da32bfd8bb005f852d15596ad75d0a
julienbrg <julien@strat.cc>
2023-08-26 15:19:24 +0200

    pokt as default provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,13 @@
 'use client'
-
+import { FleekSdk, ApplicationAccessTokenService } from '@fleekxyz/sdk';
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
+
+const applicationService = new ApplicationAccessTokenService({
+  clientId: 'client_VdSrZ14DxFUhHNeYnwfw',
+})
+
+const fleekSdk = new FleekSdk({ accessTokenService: applicationService })
 
 export function Providers({ 
     children 
