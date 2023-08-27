de3100c2228682ae1a4ae671aaa29239f3b33ef2
julienbrg <julien@strat.cc>
2023-08-27 13:28:25 +0200

    comment out fleek

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,11 +3,11 @@ import { FleekSdk, ApplicationAccessTokenService } from '@fleekxyz/sdk';
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 
-const applicationService = new ApplicationAccessTokenService({
-  clientId: 'client_VdSrZ14DxFUhHNeYnwfw',
-})
+// const applicationService = new ApplicationAccessTokenService({
+//   clientId: 'client_VdSrZ14DxFUhHNeYnwfw',
+// })
 
-const fleekSdk = new FleekSdk({ accessTokenService: applicationService })
+// const fleekSdk = new FleekSdk({ accessTokenService: applicationService })
 
 export function Providers({ 
     children 
